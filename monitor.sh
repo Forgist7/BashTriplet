d72ac123bc93e0a8ab2e1ab9ee7ce2220228c21c
Forgist7 <101464218+Forgist7@users.noreply.github.com>
2025-03-28 17:32:01 +0300

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 LOGFILE="/opt/workdir/log.txt"
-WORKDIR="/opt/workdir"
+DIRECTORY="/opt/workdir"
 SCRIPT_NAME=$(basename "$0")
 DATE=$(date '+%Y-%m-%d %H:%M:%S')
 
-echo "[$DATE] Start $SCRIPT_NAME" >> "$LOGFILE"
+echo "[$DATE] Старт $SCRIPT_NAME" >> "$LOGFILE"
 
-for user in $(ls -l "$WORKDIR" | awk '{print $3}' | sort | uniq); do
-    COUNT=$(find "$WORKDIR" -user "$user" -type f -name "*.elf" | wc -l)
-    echo "Пользователь: $user, ELF-файлов: $COUNT" >> "$LOGFILE"
+for user in $(ls -l "$DIRECTORY" | awk '{print $3}' | sort | uniq); do
+    USER_ID=$(id -u "$user" 2>/dev/null)
+    if [ -n "$USER_ID" ]; then
+        COUNT=$(find "$DIRECTORY" -user "$user" -type f -name "*.elf" | wc -l)
+        echo "Пользователь: $user (UID: $USER_ID), ELF-файлов: $COUNT" >> "$LOGFILE"
+    fi
 done
 
 echo "------" >> "$LOGFILE"
+
